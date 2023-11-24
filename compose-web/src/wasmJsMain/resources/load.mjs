22ced7dc8cf936d5a82855df76a87cbbe298889f
John O'Reilly <oreillyj@gmail.com>
2023-11-24 18:49:46 +0100

    fix issue when wasm distribution task was being used

--- compose-web/src/wasmJsMain/resources/load.mjs
+++ compose-web/src/wasmJsMain/resources/load.mjs
@@ -2,9 +2,4 @@ import { instantiate } from './Chip8.uninstantiated.mjs';
 
 await wasmSetup;
 
-let te = null;
-try {
-    await instantiate({ skia: Module['asm'] });
-} catch (e) {
-  te = e;
-}
+instantiate({ skia: Module['asm'] });
